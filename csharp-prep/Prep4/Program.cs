60cfcf6cb4cdfc01ae1d8a14fe05d22ff67a963c
technovore01101110 <1sharpnathan@gmail.com>
2025-09-22 11:46:13 -0600

    Finished prep4 assignment about using lists in C#

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int userInput;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        } while (userInput != 0);
+
+        int sum = numbers.Sum();
+        float totalNumbers = numbers.Count;
+        float average = sum / totalNumbers;
+        int largestNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
