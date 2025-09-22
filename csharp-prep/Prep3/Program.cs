78a6ec7f5df859c50c6576bb33301ead0b9aba93
technovore01101110 <1sharpnathan@gmail.com>
2025-09-22 09:25:42 -0600

    Finished Prep3 Assignment with one stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string game_play = "yes";
+        while (game_play == "yes")
+        {
+            Random randomgenerator = new Random();
+            int magic_number = randomgenerator.Next(1, 100);
+            int user_guess;
+            do
+            {
+                Console.Write("What is your guess? ");
+                user_guess = int.Parse(Console.ReadLine());
+
+                if (user_guess == magic_number)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (user_guess <= magic_number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (user_guess != magic_number);
+
+            Console.WriteLine("Would You like to play again(yes/no)? ");
+            game_play = Console.ReadLine();
+        }
     }
 }
