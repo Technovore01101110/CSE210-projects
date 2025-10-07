ca064d9b92ed28dbcb03b2deaa30f3e66bc31731
technovore01101110 <1sharpnathan@gmail.com>
2025-10-07 16:52:14 -0600

    Tested Datime class in the Sandbox for the Journal Class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,9 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        string randomWords = "hello sandbox world!!!";
-        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
-        string capitalized = textInfo.ToTitleCase(randomWords);
-        Console.WriteLine(capitalized);
+        // string randomWords = "hello sandbox world!!!";
+        // TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        // string capitalized = textInfo.ToTitleCase(randomWords);
+        // Console.WriteLine(capitalized);
+
+        DateTime localDateTime = DateTime.Now;
+        Console.WriteLine($"Local Date and Time: {localDateTime}");
     }
 }
