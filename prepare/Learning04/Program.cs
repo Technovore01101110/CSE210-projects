49a25bf712f4731c1bc1328d8aaae5c45abd7cdb
Nathan Sharp <145712080+Technovore01101110@users.noreply.github.com>
2025-10-27 18:21:31 -0600

    Finished Learning 04 Assignment for learning Inheritance.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,25 @@
 using System;
+// Inheritance design assignment.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignmentMath = new MathAssignment("Roberto Rodriguez",
+                                                           "Fractions", "3.7",
+                                                           "8-19");
+        WritingAssignment assignmentWrite = new WritingAssignment("Mary Waters",
+                                                                  "European History",
+                                                                  "The causes of World War II");
+
+        // Math Display
+        Console.WriteLine(assignmentMath.GetSummary());
+        Console.WriteLine(assignmentMath.GetHomeworkList());
+        Console.WriteLine();
+
+        // Writing Display
+        Console.WriteLine(assignmentWrite.GetSummary());
+        Console.WriteLine(assignmentWrite.GetWritingInformation());
+
     }
 }
